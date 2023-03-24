dd21d66ccf3de4d1c0eb001832470887646afcee
Halias3422 <v.desaintexupery@gmail.com>
2023-03-24 11:21:54 +0100

    feat(favicon): add custom favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Favicon from "@/components/Favicon";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        <Favicon />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
