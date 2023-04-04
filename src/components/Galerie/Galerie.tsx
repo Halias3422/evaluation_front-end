9b3af77ee8a2c8dfa85dc7e461192a2f4ad3f38d
Halias3422 <v.desaintexupery@gmail.com>
2023-04-04 13:44:16 +0200

    test(gallery): revert to remove fullscreen image transition

--- src/components/Galerie/Galerie.tsx
+++ src/components/Galerie/Galerie.tsx
@@ -109,18 +109,6 @@ const Galerie = ({ photos }: { photos: Photo[]; categories: Category[] }) => {
     });
   };
 
-  const handleImageTransition = () => {
-    const image = document.getElementById(
-      "fullScreenImage"
-    ) as HTMLImageElement;
-    if (image) {
-      image.addEventListener("animationend", () => {
-        image.src = fullScreenPhoto?.imageBig.image || "";
-        image.srcset = fullScreenPhoto?.imageBig.image || "";
-      });
-    }
-  };
-
   return (
     <>
       <div id="galleryPage" className="pageContainer">
@@ -150,11 +138,10 @@ const Galerie = ({ photos }: { photos: Photo[]; categories: Category[] }) => {
           <FullScreenImage
             priority
             id="fullScreenImage"
-            src={fullScreenPhoto.imageSmall.image}
+            src={fullScreenPhoto.imageBig.image}
             alt={fullScreenPhoto.title}
             width={fullScreenPhoto.imageBig.width}
             height={fullScreenPhoto.imageBig.height}
-            onLoad={handleImageTransition}
           />
         </PopUpBackground>
       )}
